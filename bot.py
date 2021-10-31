017bb2e4a82010eb1365ca97706d3365a39da609
Code X Mania <88939380+Code-X-Mania@users.noreply.github.com>
2021-10-31 14:18:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -78,13 +78,13 @@ async def _(event):
             username = f"@{uu}"
         else:
             username = mention
-        x = await get_user_join(user.id)
+        g = await get_user_join(user.id)
         if x is True:
             msg = welcome_msg.format(mention=mention, title=title, fullname=fullname, username=username, name=name, last=last, channel=f"@{channel}")
             butt = [Button.url("Channel", url=f"https://t.me/{channel}")]
         else:
             msg = welcome_not_joined.format(mention=mention, title=title, fullname=fullname, username=username, name=name, last=last, channel=f"@{channel}")
-            butt = [Button.url("Channel", url=f"https://t.me/{channel}"), Button.inline("UnMute Me", data=f"unmute_{user.id}")]
+            butt = [Button.url("Channel", url=f"https://t.me/{channel}"), Button.inline("UnMute Me", data=f"unmute_me{user.id}")]
             await codexmania.edit_permissions(event.chat.id, user.id, until_date=None, send_messages=False)
         
         await event.reply(msg, buttons=butt)
@@ -96,7 +96,7 @@ async def mute_on_msg(event):
         return
     if on_new_msg is False:
         return
-    x = await get_user_join(event.sender_id)
+    g = await get_user_join(event.sender_id)
     temp = await codexmania(GetFullUserRequest(event.sender_id))
     if x is False:
         if temp.user.bot:
@@ -110,15 +110,15 @@ async def mute_on_msg(event):
         await event.reply(f"Hey {nm}, seems like you haven't joined our channel. Please join @{channel} and then press the button below to unmute yourself!", buttons=[[Button.url("Channel", url=f"https://t.me/{channel}")], [Button.inline("UnMute Me", data=f"unmute_{event.sender_id}")]])
 
 
-@codexmania.on(events.callbackquery.CallbackQuery(data=re.compile(b"unmute_(.*)")))
+@codexmania.on(events.callbackquery.CallbackQuery(data=re.compile(b"unmute_me(.*)")))
 async def _(event):
     uid = int(event.data_match.group(1).decode("UTF-8"))
     if uid == event.sender_id:
-        x = await get_user_join(uid)
+        g = await get_user_join(uid)
         nm = (await codexmania(GetFullUserRequest(uid))).user.first_name
-        if x is False:
+        if g is False:
             await event.answer(f"You haven't joined @{channel} yet!", cache_time=0, alert=True)
-        elif x is True:
+        elif g is True:
             try:
                 await codexmania.edit_permissions(event.chat.id, uid, until_date=None, send_messages=True)
             except Exception as e:
